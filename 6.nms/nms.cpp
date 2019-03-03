0a9119f0c934e5c21aa17bd9a33405b9bb336fd8
dupeng <dupeng63@163.com>
2019-03-04 07:02:50 +0800

    添加softnms论文中的原理图，修改注释使其更准确

--- 6.nms/nms.cpp
+++ 6.nms/nms.cpp
@@ -34,7 +34,7 @@ float iou(const std::vector<float> boxA, const std::vector<float> boxB)
  * boxes，输入，原始框信息组成的向量，其中每个元素都是一个原始框的信息，
  *        这些信息依次以x_topleft,y_topleft,x_bottomright,ybuttomright,confidence为顺序
  * threshold，输入，置信度阈值，仅保留置信度大于它的原始框
- * type， 输入，nms类型，normal表示用常见的nms，soft表示用soft nms，默认normal
+ * type， 输入，nms类型（暂时只有normal），normal表示用常见的nms，soft表示用soft nms，默认normal
  * 输出：nms算法的返回值，是一个向量，向量中的每一个元素表示保留下来的原始框在输入boxes中的下标
  *       如果输入boxes空，则返回为空
  *
@@ -54,11 +54,11 @@ std::vector<int> nms(const std::vector<std::vector<float>> boxes, const float th
     std::sort(list_k.begin(), list_k.end(),
               [&](int i1, int i2) { return boxes[i1][4] < boxes[i2][4]; });
 
-    std::vector<int> pick;
+    std::vector<int> keep;
     while (!list_k.empty()) {
         // 保留本轮最高分的index
         const int top_1 = list_k.back();
-        pick.push_back(top_1);
+        keep.push_back(top_1);
 
         // 删除最高分的index
         list_k.pop_back();
@@ -72,6 +72,6 @@ std::vector<int> nms(const std::vector<std::vector<float>> boxes, const float th
         }
     }
 
-    return pick;
+    return keep;
 }
 

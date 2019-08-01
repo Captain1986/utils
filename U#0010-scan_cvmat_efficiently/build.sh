2a6383e8d08e64b03ca9aa1053fa91a8675167f3
dupeng <dupeng63@163.com>
2019-08-01 18:24:05 +0800

    添加OpenCV的image.forEach扫描图片的方法

--- U#0010-scan_cvmat_efficiently/build.sh
+++ U#0010-scan_cvmat_efficiently/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-g++ main.cpp `pkg-config --libs --cflags opencv` -O0
+g++ main.cpp `pkg-config --libs --cflags opencv` -O0 -std=c++11
--- U#0010-scan_cvmat_efficiently/main.cpp
+++ U#0010-scan_cvmat_efficiently/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <assert.h>
+#include <atomic>
 #include "opencv2/core/core.hpp"
 
 using namespace std;
@@ -13,6 +14,7 @@ void method5_2(cv::Mat);
 void method5_3(cv::Mat); // 最快的
 void method6(cv::Mat);
 void method7(cv::Mat);
+void method8(cv::Mat);
 
 int main(int argc, char* argv[])
 {
@@ -27,6 +29,7 @@ int main(int argc, char* argv[])
     method5_3(image);
     method6(image);
     method7(image);
+    method8(image);
 }
 
 void method1(cv::Mat img){
@@ -170,6 +173,8 @@ void method5_3(cv::Mat img){
     if(img.isContinuous()) {
        width = width * height;
        height = 1;
+    } else {
+       printf("img.isContinuous() is false\n");
     }
 
     int sum = 0;
@@ -223,4 +228,48 @@ void method7(cv::Mat image){
     assert(sum==3*height*width);
     double time = ((double) cv::getTickCount() - t0) / cv::getTickFrequency();
     std::cout << "Time for method7: " << time << std::endl;
+}
+
+// C++11 Lambda
+//typedef cv::Point3_<uint8_t> Pixel;
+typedef cv::Vec3b Pixel;
+
+void method8(cv::Mat image){
+    if(image.isContinuous()) {
+       printf("img.isContinuous() is true\n");
+    } else {
+       printf("img.isContinuous() is false\n");
+    }
+    double t0 = (double) cv::getTickCount();
+    int height = image.rows;
+    int width = image.cols;
+    // 用forEach，要小心Data Race!所以不一定高效的，反而有可能是最慢的
+#if 1
+    std::atomic<int> num;
+    std::atomic<int> sum;
+#else
+    // 这个是不能用的，会有Data Race!
+    int num, sum;
+#endif
+    num = 0;
+    sum = 0;
+    image.forEach<Pixel>(
+         [&](Pixel &pixel, const int * position) -> void {
+           if(pixel[0] != 1 or pixel[1] != 1 or pixel[2] != 1) {
+              printf("pixel[0] = %d, pixel[1] = %d, pixel[2] = %d\n", pixel[0], pixel[1], pixel[2]);
+           }
+           //sum += pixel.x + pixel.y + pixel.z;
+           sum += pixel[0] + pixel[1] + pixel[2];
+           num++;
+         }
+    );
+#if 1
+    printf("num = %d, sum = %d, sum/3 = %d, 3*height*width = %d\n", num.load(), sum.load(), sum.load()/3, 3*height*width);
+    assert(sum.load()==3*height*width);
+#else
+    printf("num = %d, sum = %d, sum/3 = %d, 3*height*width = %d\n", num, sum, sum/3, 3*height*width);
+    assert(sum==3*height*width);
+#endif
+    double time = ((double) cv::getTickCount() - t0) / cv::getTickFrequency();
+    std::cout << "Time for method8: " << time << std::endl;
 }

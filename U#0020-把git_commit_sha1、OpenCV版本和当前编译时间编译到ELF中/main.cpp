1df307ac16dbe2fc33788a92033dddd7c5c3393a
dupeng <dupeng63@163.com>
2020-01-11 17:21:17 +0800

    格式调整

--- U#0020-把git_commit_sha1、OpenCV版本和当前编译时间编译到ELF中/main.cpp
+++ U#0020-把git_commit_sha1、OpenCV版本和当前编译时间编译到ELF中/main.cpp
@@ -10,10 +10,10 @@
 
 int main(int argc, char** argv)
 {
-	// 打印ELF版本和当前编译时间
+    // 打印ELF版本和当前编译时间
     std::cout << "Welcome to use " << VERSION_STRING << std::endl;
 
-	// 打印git commit sha1信息
+    // 打印git commit sha1信息
     printf("git_commit_sha1:\t%s\n", GIT_COMMIT_SHA1);
 
     // 打印OpenCV版本信息（从头文件中拿）
